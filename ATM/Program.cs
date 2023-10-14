57fb031efc6edb852666f7875fa4871762c35564
Isac Elfstrand <isac.elfstrand@edu.varberg.se>
2023-10-14 16:51:34 +0200

    Nu är koden klar i funktionerna och när man använder menyn ska den tillropa funktionerna som är för varje val.

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -97,29 +97,135 @@ namespace ATM
 
         static void Accounts(string username, string[] accountTypes, int currentUserIndex, double[] balances)
         {
-
+            Console.WriteLine($"Here are your accounts, {username}:");
+            for (int i = 0; i < accountTypes.Length; i++)
+            {
+                Console.WriteLine($"{accountTypes[i]}: {balances[currentUserIndex * accountTypes.Length + i]}$");
+            }
         }
 
         static void TransferMoney(string username, string[] accountTypes, int currentUserIndex, double[] balances)
         {
+            if (currentUserIndex >= 0)
+            {
+                int userBalanceIndex = currentUserIndex * accountTypes.Length;
 
+                Console.WriteLine("Your accounts:");
+                for (int i = 0; i < accountTypes.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {accountTypes[i]}: {balances[userBalanceIndex + i]}$");
+                }
+
+                Console.Write("Select the account to transfer from (Enter the number): ");
+                int sourceAccountIndex = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                if (sourceAccountIndex >= 0 && sourceAccountIndex < accountTypes.Length)
+                {
+                    Console.Write("Enter the amount to transfer: ");
+                    double amountToTransfer = Convert.ToDouble(Console.ReadLine());
+
+                    if (amountToTransfer <= balances[userBalanceIndex + sourceAccountIndex])
+                    {
+                        Console.WriteLine("Your account for transfer:");
+                        for (int i = 0; i < accountTypes.Length; i++)
+                        {
+                            if (i != sourceAccountIndex)
+                            {
+                                Console.WriteLine($"{i + 1}. {accountTypes[i]}");
+                            }
+                        }
+                        Console.Write("Select the account to transfer to (Enter the number): ");
+                        int targetAccountIndex = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                        if (targetAccountIndex >= 0 && targetAccountIndex < accountTypes.Length)
+                        {
+                            balances[userBalanceIndex + sourceAccountIndex] -= amountToTransfer;
+                            balances[userBalanceIndex + targetAccountIndex] += amountToTransfer;
+
+                            Console.WriteLine($"Transfer of {amountToTransfer}$ from {accountTypes[sourceAccountIndex]} to {accountTypes[targetAccountIndex]} completed.");
+
+                            Console.WriteLine("Your updated balances:");
+                            for (int i = 0; i < accountTypes.Length; i++)
+                            {
+                                Console.WriteLine($"{accountTypes[i]}: {balances[userBalanceIndex + i]}$");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid target account selection.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You do not have enough funds in your {accountTypes[sourceAccountIndex]} account.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid source account selection.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No accounts found for this user.");
+            }
         }
 
-        static void WithdrawMoney(string username, string[] accountTypes, int currentUserIndex, double[] balances, string[] pins)
+    static void WithdrawMoney(string username, string[] accountTypes, int currentUserIndex, double[] balances, string[] pins)
         {
+            if (currentUserIndex >= 0)
+            {
+                int userBalanceIndex = currentUserIndex * accountTypes.Length;
 
+                Console.WriteLine("Choose the account to withdraw from:");
+                for (int i = 0; i < accountTypes.Length; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {accountTypes[i]}");
+                }
+                int selectedAccountIndex = Convert.ToInt32(Console.ReadLine()) - 1;
+
+                if (selectedAccountIndex >= 0 && selectedAccountIndex < accountTypes.Length)
+                {
+                    Console.Write($"Enter the amount to withdraw from {accountTypes[selectedAccountIndex]} account: ");
+                    double amountToWithdraw = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine("Please enter your PIN Code to confirm the withdrawal: ");
+                    string enteredPin = Console.ReadLine();
+
+                    if (enteredPin == pins[currentUserIndex])
+                    {
+                        if (amountToWithdraw <= balances[userBalanceIndex + selectedAccountIndex])
+                        {
+                            balances[userBalanceIndex + selectedAccountIndex] -= amountToWithdraw;
+                            Console.WriteLine($"You have withdrawn {amountToWithdraw}$ from {accountTypes[selectedAccountIndex]} account.");
+                            Console.WriteLine($"Your updated balance for {accountTypes[selectedAccountIndex]}: {balances[userBalanceIndex + selectedAccountIndex]}$");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"You do not have enough funds in your {accountTypes[selectedAccountIndex]} account.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect PIN. Withdrawal canceled.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid account selection.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No accounts found for this user.");
+            }
         }
 
         static void LogOut()
         {
-
-        }
-
-
-
-
-
-
-        }
+            Console.WriteLine("You are logged out");
+            loggedIn = false;
+        }  
     }
 }  
