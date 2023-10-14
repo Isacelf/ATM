569b885119ed62e046720a63056fff9464455c8d
Isac Elfstrand <isac.elfstrand@edu.varberg.se>
2023-10-14 16:25:18 +0200

    Börjar med att skapa alla arrays, variabler och placeholders som är grunden i mitt program.

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -3,9 +3,20 @@ namespace ATM
 {
     internal class Program
     {
+        static bool loggedIn = false;
+        static double[] balances = { 2000, 320.45, 160.8, 790, 577.8, 7000, 456.3, 6000, 731.78, 2500 };
+
         static void Main(string[] args)
         {
-            
+            string[] usernames = { "admin", "user1", "user2", "user3", "user4" };
+            string[] pins = { "2000", "7777", "1555", "1234", "9898" };
+            string[] accountTypes = { "Save Account", "Pay Account" };
+            Console.WriteLine("Hello and welcome to the bank!");
+
+            int attempts = 3;
+            var username = "";
+            var pin = "";
+            int currentUserIndex = -1;
 
         }
     }
